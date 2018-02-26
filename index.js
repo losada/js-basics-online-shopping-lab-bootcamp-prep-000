407fead88bd81b7467d35dd93f85f96a3f054ff0
Michael Losada <michael.losada@gmail.com>
2018-02-26 18:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber === null){
    console.log('Sorry, we don\'t have a credit card on file for you.');}
+   
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   cart = [];
 }
